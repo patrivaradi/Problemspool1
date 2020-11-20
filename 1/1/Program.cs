7feb227c04de087b6c1aa9bb6fbe56d40bb87ae9
Patricia Varadi <varadi.patricia@student.uoradea.ro>
2020-11-20 17:27:12 +0200

    ax+b=0

--- 1/1/Program.cs
+++ 1/1/Program.cs
@@ -7,9 +7,23 @@ using System.Threading.Tasks;
 namespace _1
 {
     class Program
-    {        
+    {    
+        /// <summary>
+        /// Rezolvati ecuatia de gradul 1 cu o necunoscuta: ax+b = 0, unde a si b sunt date de intrare. 
+        /// </summary>
         static void Main(string[] args)
-        {            
+        {
+            int a, b, e;
+            Console.WriteLine("Ecuatie de gradul 1:  ax+b=0 ");
+            Console.Write("Introduceti a=");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Introduceti b=");
+            b = int.Parse(Console.ReadLine());
+            e = (-b) / a;
+            Console.WriteLine();
+            Console.WriteLine($"{a} x + {b} = 0");
+            Console.WriteLine($"x = {e}");
+            Console.ReadKey();
         }
     }
 }
