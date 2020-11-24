13dab483592522889e570e9f4f63f979ec713f08
Patricia Varadi <varadi.patricia@student.uoradea.ro>
2020-11-24 19:56:27 +0200

    swap a with b

---
+++ 7/7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7
+{
+    class Program
+    {
+        /// <summary>
+        /// (Swap) Se dau doua variabile numerice a si b ale carori valori sunt date de intrare. 
+        /// Se cere sa se inverseze valorile lor. 
+        /// </summary>
+        static void Main(string[] args)
+        {
+            int a, b, aux=0;
+            Console.Write("Introduceti a:");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Introduceti b:");
+            b = int.Parse(Console.ReadLine());
+
+            aux = a;
+            a = b;
+            b = aux;
+
+            Console.WriteLine(a+" "+b);
+            Console.ReadKey();
+        }
+    }
+}
