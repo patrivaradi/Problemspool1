b9f81cdd64828f789b9e35e8879f7fad55a6df1e
Patricia Varadi <varadi.patricia@student.uoradea.ro>
2020-11-24 20:18:23 +0200

    Divisible by..

---
+++ 9/9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9
+{
+    class Program
+    {
+        /// <summary>
+        /// Afisati toti divizorii numarului n. 
+        /// </summary>
+
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Introduceti n=");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Divizorii lui {n} sunt:");
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                    Console.Write(i+" ");                
+            }
+            Console.ReadKey();
+        }
+    }
+}
