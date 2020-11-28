7764d27f610d4375ed963591224ffdb91ff63df3
Patricia Varadi <varadi.patricia@student.uoradea.ro>
2020-11-28 18:04:44 +0200

    decomposition into prime factors

---
+++ 18/18/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18
+{
+    class Program
+    {
+        /// <summary>
+        /// Afisati descompunerea in factori primi ai unui numar n.  De ex. pentru n = 1776 afisati 2^3 x 3^1 x 7^2. 
+        /// </summary>
+        static void Main(string[] args)
+        {
+            int n, div = 2, exp=0;
+            Console.Write("Introduceti numarul : ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Descompunerea in factori primi este :");
+            while (n != 0)
+            {
+                while (n % div == 0)
+                {                    
+                    n = n / div;
+                    exp++;
+                }
+                if (exp != 0)
+                    Console.Write($"{div}^{exp} x ");
+                div++;
+                exp = 0;
+            }            
+            Console.ReadKey();
+        }
+    }
+}
