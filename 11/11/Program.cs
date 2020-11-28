c4994da198725162c686b1223e0132b6a515b82f
Patricia Varadi <varadi.patricia@student.uoradea.ro>
2020-11-28 13:39:52 +0200

    inverted nr.

---
+++ 11/11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11
+{
+    class Program
+    {
+        /// <summary>
+        /// Afisati in ordine inversa cifrele unui numar n. 
+        /// </summary>
+        static void Main(string[] args)
+        {
+            int n,m;
+            Console.Write("Introduceti  n = ");
+            n = int.Parse(Console.ReadLine());
+            m = n;
+            while (m != 0) 
+            {
+                Console.Write(m % 10);
+                m /= 10;
+            }            
+            Console.ReadKey();
+        }
+    }
+}
