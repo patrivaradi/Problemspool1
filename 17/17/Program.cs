a7e246cac7d2a9d933a7209a39ae4e723758ebff
Patricia Varadi <varadi.patricia@student.uoradea.ro>
2020-11-28 17:39:13 +0200

    Euclid algorithm

---
+++ 17/17/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17
+{
+    class Program
+    {
+        /// <summary>
+        /// Determianti cel mai mare divizor comun si cel mai mic multiplu comun a doua numere. Folositi algoritmul lui Euclid. 
+        /// </summary>
+        static void Main(string[] args)
+        {
+            int n1, n2,aux1,aux2,aux3,aux4;
+            Console.WriteLine("Introduceti numerele : ");
+            n1 = int.Parse(Console.ReadLine());
+            n2 = int.Parse(Console.ReadLine());
+            aux1 =aux3= n1; 
+            aux2=aux4 = n2;
+            while (aux1 != aux2)
+            {
+                if (aux1 > aux2)
+                    aux1 -= aux2;
+                else
+                    aux2 -= aux1;
+            }
+            Console.WriteLine($"Cel mai mare divizor comun este : {aux1}");            
+            while(aux3!=aux4)
+            {
+                if (aux3 < aux4)
+                    aux3 += n1;
+                else
+                    aux4 += n2;
+            }
+            Console.WriteLine($"Cel mai mic multiplu comun este : {aux3}");
+            Console.ReadKey();
+        }
+    }
+}
