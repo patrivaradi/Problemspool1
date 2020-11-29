0b01d626a93aab52def5f05ffc122a0bf72877d7
Patricia Varadi <varadi.patricia@student.uoradea.ro>
2020-11-29 15:26:29 +0200

    fraction m/n with ..(..)

---
+++ 20/20/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20
+{
+    class Program
+    {
+        /// <summary>
+        /// Afisati fractia m/n in format zecimal, cu perioada intre paranteze (daca e cazul). Exemplu: 13/30 = 0.4(3). 
+        /// </summary>
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
