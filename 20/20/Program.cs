0288d9bd784f623c4997ffd6806d78cf51703f17
Patricia Varadi <varadi.patricia@student.uoradea.ro>
2020-12-08 13:13:55 +0200

    m / n = ..,..(..)

--- 20/20/Program.cs
+++ 20/20/Program.cs
@@ -13,14 +13,53 @@ namespace _20
         /// </summary>
         static void Main(string[] args)
         {
-            int m, n, intraga,zecimal;
+            int m, n,intreaga,zecimal;
             Console.Write("Introduceti  m = ");
             m = int.Parse(Console.ReadLine());
             Console.Write("Introduceti  n = ");
             n = int.Parse(Console.ReadLine());
-            intraga = m / n;
+            intreaga = m / n;
             zecimal = m % n;
-            Console.WriteLine($"{m}/{n} = {intraga}.{zecimal}");
+            Console.Write($"{intreaga},");
+            int cifra, rest;
+            List<int> resturi = new List<int>();
+            List<int> cifre = new List<int>();
+            resturi.Add(zecimal);
+            bool periodic = false;
+            do
+            {
+                cifra = zecimal * 10 / n;
+                cifre.Add(cifra);
+                rest = zecimal * 10 % n;
+                if (!resturi.Contains(rest))
+                {
+                    resturi.Add(rest);
+                }
+                else
+                {
+                    periodic = true;
+                    break;
+                }                
+                zecimal = rest;
+            } while (zecimal != 0);
+            if(!periodic)
+            {
+                foreach (var item in cifre)
+                {
+                    Console.Write(item);
+                }
+            }
+            else 
+            {
+                for (int i = 0; i < resturi.Count; i++)
+                {
+                    if (resturi[i] == rest)
+                        Console.Write("(");
+                    Console.Write(cifre[i]);
+                }
+                Console.Write(")");
+            }
+
             Console.ReadKey();
         }
     }
